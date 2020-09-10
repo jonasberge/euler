d0831d494cd3cb9aa45590dfec59693f3c57279e
vonas <24623262+vonas@users.noreply.github.com>
2020-09-10 16:25:11 +0200

    Add template and automating script

---
+++ create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://stackoverflow.com/a/4774063/6748004
+DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DESK="$DIR/desk"
+
+PROBLEM=$1
+
+SRC="$DIR/template.py"
+DEST="$DESK/$PROBLEM.py"
+
+if [ -f "$DEST" ]; then
+	echo "$DEST already exists"
+	exit 1
+fi
+
+mkdir -p "$DESK"
+sed -E "2s/(problem=)\?/\1$PROBLEM/g" "$SRC" > "$DEST"
