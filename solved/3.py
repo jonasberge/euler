ccfc1f87b401f23a152a4caabaa37285f55789e6
vonas <24623262+vonas@users.noreply.github.com>
2020-09-10 23:47:33 +0200

    Use sieve from the utility module

--- solved/3.py
+++ solved/3.py
@@ -19,31 +19,7 @@ to find those prime numbers we can utilize the sieve of eratosthenes.
 
 from math import sqrt
 
-
-def sieve(n):
-    n = n + 1  # our indices are based at 1, not 0
-
-    table = [True] * n
-    table[0] = table[1] = False  # 0 and 1 are not prime numbers
-
-    i = 2
-    while i < n:
-        for k in range(i + i, n, i):
-            table[k] = False
-
-        for j in range(i + 1, n):
-            if table[j]:
-                i = j
-                break
-
-        if i != j:
-            break
-
-    return [
-        n
-        for n, is_prime in enumerate(table)
-        if is_prime
-    ]
+from util import sieve
 
 
 def solve(n):
