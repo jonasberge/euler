f492022f9fbadf7faf6dac663832da673f922cde
vonas <24623262+vonas@users.noreply.github.com>
2020-09-20 20:10:40 +0200

    Add solution for problem 67

---
+++ solved/67.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# https://projecteuler.net/problem=67
+
+
+from solved import p18
+
+
+# use a lambda such that the call stack
+# for read_data() includes this file.
+solve = lambda: p18.solve()
+
+
+args = ()
+solution = 7273
