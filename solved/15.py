a85d94f816070f3b26e9e65f6f086ea444cc35ba
vonas <24623262+vonas@users.noreply.github.com>
2020-09-20 17:13:28 +0200

    Add solution for problem 15

---
+++ solved/15.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# https://projecteuler.net/problem=15
+
+
+from math import factorial
+
+
+def binomial(n, k):
+    return factorial(n) // factorial(k) // factorial(n - k)
+
+
+def solve(n, m):
+    return binomial(n + m, n)
+
+
+args = (20, 20)
+solution = None
